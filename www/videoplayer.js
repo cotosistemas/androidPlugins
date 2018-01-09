bb65dff2a8f83975f83e75a59cf2f65ca77ddf59
CENTRAL\147855 <ariel.rivenson@redcoto.com.ar>
2018-01-09 10:37:58 -0300

    fix

--- www/videoplayer.js
+++ www/videoplayer.js
@@ -1,34 +1,38 @@
+cordova.define("cordova/plugin/VideoPlayer", function(require, exports, module) {      
+
 var exec = require("cordova/exec");
+var VideoPlayer = function () {};
 
-module.exports = {
-
-    DEFAULT_OPTIONS: {
+	VideoPlayer.prototype.DEFAULT_OPTIONS: {
         volume: 1.0,
         scalingMode: 1
-    },
+    };
 
-    SCALING_MODE: {
+    VideoPlayer.prototype.SCALING_MODE: {
         SCALE_TO_FIT: 1,
         SCALE_TO_FIT_WITH_CROPPING: 2
-    },
-
-    play: function (path, options, successCallback, errorCallback) {
-        options = this.merge(this.DEFAULT_OPTIONS, options);
+    };
+	
+	VideoPlayer.prototype.play = function(path, options, successCallback, errorCallback) {
+				 options = this.merge(this.DEFAULT_OPTIONS, options);
         exec(successCallback, errorCallback, "VideoPlayer", "play", [path, options]);
-    },
+	};
 
-    close: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "VideoPlayer", "close", []);
-    },
+	VideoPlayer.prototype.close = function(notificationId) {
+		exec(successCallback, errorCallback, "VideoPlayer", "close", []);
+	};
 
-    merge: function () {
-        var obj = {};
-        Array.prototype.slice.call(arguments).forEach(function(source) {
+	VideoPlayer.prototype.merge = function() {
+		 var obj = {};
+         Array.prototype.slice.call(arguments).forEach(function(source) {
             for (var prop in source) {
                 obj[prop] = source[prop];
             }
         });
         return obj;
-    }
+	};
 
-};
+
+	var VideoPlayer = new VideoPlayer();
+	module.exports = VideoPlayer
+});
