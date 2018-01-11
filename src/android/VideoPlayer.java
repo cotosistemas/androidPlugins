fca1d51a797bd086c0c7ab1b550bfe19d7462d5c
CENTRAL\147855 <ariel.rivenson@redcoto.com.ar>
2018-01-11 16:27:37 -0300

    fix

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -164,7 +164,7 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
             }
         });
 		Log.v(LOG_TAG, path);
-		path = path.replaceAll("]","").replaceAll("\"","");
+		path = path.replaceAll("[","").replaceAll("\"","").replaceAll("|","/");
 		Log.v(LOG_TAG, path);
 		Uri uri= Uri.parse(path);
         videoView.setVideoURI(uri);
